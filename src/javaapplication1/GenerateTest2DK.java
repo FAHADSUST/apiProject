d4bc10ade3da776becc62044d46bfeeca1fa4a88
Fahad <fahad@ipvisionsoft.com>
2017-04-12 12:32:31 +0600

    new kuasha episode added

--- src/javaapplication1/GenerateTest2DK.java
+++ src/javaapplication1/GenerateTest2DK.java
@@ -146,7 +146,7 @@ public class GenerateTest2DK {
         String start = "12/08/2010";
         String end = "01/02/2011";
         DateTimeFormatter pattern = DateTimeFormat.forPattern("dd/mm/yyyy");
-        DateTime startDate = new DateTime(2017, 4, 5, 0, 0, 0, 0);//pattern.parseDateTime(start); // year-month-day
+        DateTime startDate = new DateTime(2017, 4, 12, 0, 0, 0, 0);//pattern.parseDateTime(start); // year-month-day
         DateTime endDate = new DateTime(2010, 1, 1, 0, 0, 0, 0);//pattern.parseDateTime(end);
 
         
@@ -155,7 +155,7 @@ public class GenerateTest2DK {
         int yearID = 0;
         int albumID = 0;
         int songID = 0;
-        int episodeNumber = 205;
+        int episodeNumber = 206;
         String url = "{\"albumList\":[";
         String prevYear = "";
         
