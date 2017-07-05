2d67375a77254f21c0ae323c931b38be4a45889e
Fahad <mdashfahad@gmail.com>
2017-07-05 20:37:26 +0600

    sunday suspense new file added

--- src/javaapplication1/GenerateTest2Suspense.java
+++ src/javaapplication1/GenerateTest2Suspense.java
@@ -5,7 +5,6 @@
  */
 package javaapplication1;
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -24,7 +23,6 @@ import org.joda.time.DateTimeConstants;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-
 /**
  *
  * @author Fahad-PC
@@ -37,104 +35,78 @@ public class GenerateTest2Suspense {
 //    {"firstName":"Peter", "lastName":"Jones"}
 //]}
 //    
-    
-    
-        static int songID;
-        static String title;
-	static String artist;
-	//String album;
-	static String path;
-	static long duration;
-	static long albumId;
-	static String composer;
-        static String imageUrl;
-    
-        static int albumID;
-        static String albumName;
+
+    static int songID;
+    static String title;
+    static String artist;
+    //String album;
+    static String path;
+    static long duration;
+    static long albumId;
+    static String composer;
+    static String imageUrl;
+
+    static int albumID;
+    static String albumName;
     private static Object fis;
 
     private static void readFile() {
         BufferedReader buffreader = null;
         try {
-            
-            //File file = new File("filename_sunday_sus.txt");
-            File file = new File("filename_sunday_sus_name.txt");
 
-            if (!file.exists()) {
-                    file.createNewFile();
-            }
-
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            
-            //InputStream fis = new FileInputStream("sunday_suspense.txt");     
-            InputStream fis = new FileInputStream("filename_sunday_sus.txt");  
-            // if file the available for reading
+            InputStream fis = new FileInputStream("sunday_outputfileName2.txt");
             if (fis != null) {
 
-              // prepare the file for reading
-              InputStreamReader chapterReader = new InputStreamReader(fis);
-              buffreader = new BufferedReader(chapterReader);
-              
-              String line = null;
-              while(true)
-              {     try{
-                    line = buffreader.readLine();
-                    String segment = line.substring(46);//33
-                    String fin = segment.substring(0,segment.length()-4);
-                    fin = fin.replace("_", " ");
-                    fin = fin.replace("-", " ");
-                    fin = fin.replace("%28", "");
-                    fin = fin.replace("%27", "");
-                    fin = fin.replace("%29", "");
-                    bw.write( fin+"\n");
-                    
-                    }catch(Exception e)
-                    {
+                // prepare the file for reading
+                InputStreamReader chapterReader = new InputStreamReader(fis);
+                buffreader = new BufferedReader(chapterReader);
+
+                String line = null;
+                while (true) {
+                    try {
+                        line = buffreader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        String segment = line.substring(43);//33
+                        String fin = segment.substring(0, segment.length() - 4);
+                        fin = fin.replace("_", " ");
+                        fin = fin.replace("-", " ");
+                        fin = fin.replace("%28", "");
+                        fin = fin.replace("%27", "");
+                        fin = fin.replace("%29", "");
+
+                        
+
+                        songPathList.add(line);
+                        songNameList.add(fin);
+                        
+
+                    } catch (Exception e) {
 
                     }
+                }
 
-                    if(line == null) break;
-                    
-                    songPath.add(line);
-            
-              }
-              
-              bw.close();
-              
-
-              /*String line;
-
-              // read every line of the file into the line-variable, on line at the time
-              do {
-                 line = buffreader.readLine();
-                // do something with the line 
-                 System.out.println(line);
-              } while (line != null);
-                      */
 
             }
         } catch (Exception e) {
-            
+
         } finally {
-            
+
         }
-        boolean abc = true;
-        if(abc)
-        {
-            return ;
-        }
+        
+        System.out.println("line: 1" );
     }
-        ArrayList<String> songList = new ArrayList<>();
-        static ArrayList<String> songPath = new ArrayList<>();
-        
+    ArrayList<String> songList = new ArrayList<>();
+    static ArrayList<String> songPathList = new ArrayList<>();
+    static ArrayList<String> songNameList = new ArrayList<>();
+
     //http://dl.bhoot-fm.com/Bhoot-FM_2016-10-28_(Bhoot-FM.com).mp3
-    public static void main(String[] args) throws IOException
-    {
-        
-        //readFile();
-        
-        
+    public static void main(String[] args) throws IOException {
+
+        readFile();
+        System.out.println("line: " );
+
         String[] urlImage = {
             "http://i.imgur.com/DMmPf5f.jpg",
             "http://i.imgur.com/8C3N9lN.jpg",
@@ -142,83 +114,56 @@ public class GenerateTest2Suspense {
             "http://i.imgur.com/nyv8gOD.jpg",
             "http://i.imgur.com/kzr9p5M.jpg",
             "http://i.imgur.com/nBTPbty.jpg",
-                "http://i.imgur.com/VvLI4sY.jpg"
-                
+            "http://i.imgur.com/VvLI4sY.jpg"
+
         };
+
+        String songPath = null;
+        String songName = null;
+
+        String url = "{\"songList\":[";
+
+        songID = 0;
+
+        artist = "Sunday Suspense";
+        composer = "Radio Misri";
+
+        Random r = new Random();
+
+        int i = 0;
         
-        BufferedReader buffreader;
-        BufferedReader buffreader2;
-        try {
-            
-            //File file = new File("filename_sunday_sus.txt");
-            
-            //InputStream fis = new FileInputStream("sunday_suspense.txt");     
-            InputStream fis = new FileInputStream("filename_sunday_sus.txt");  
-            InputStream fis2 = new FileInputStream("filename_sunday_sus_name.txt");  
-            // if file the available for reading
-            if (fis != null) {
+        
+        while (i < songPathList.size()) {
 
-                // prepare the file for reading
-                InputStreamReader chapterReader = new InputStreamReader(fis);
-                buffreader = new BufferedReader(chapterReader);
+            songPath = songPathList.get(i);
+            songName = songNameList.get(i);
 
-                InputStreamReader chapterReader2 = new InputStreamReader(fis2);
-                buffreader2 = new BufferedReader(chapterReader2);
+            imageUrl = urlImage[r.nextInt(7)];
 
-                String songPath = null;
-                String songName = null;
+            url += "{\"songID\":\"" + songID + "\", \"title\":\"" + (songID + 1) + "-" + songName + "\", \"artist\":\"" + artist + "\", \"path\":\"" + songPath + "\", \"albumId\":\"" + albumID + "\", \"composer\":\"" + composer + "\", \"imageUrl\":\"" + imageUrl + "\"},";
 
-                String url = "{\"songList\":[";
+            songID++;
+            i++;
 
-                songID = 0;
-
-                artist = "Sunday Suspense";
-                composer = "Radio Misri";
-
-                Random r = new Random();
-
-                while(true)
-                {     
-
-                      songPath = buffreader.readLine();
-                      songName = buffreader2.readLine();
-
-                      if(songPath == null) break;
-
-                      imageUrl = urlImage[r.nextInt(7)];
-
-                      url += "{\"songID\":\""+songID+"\", \"title\":\""+(songID+1)+"-"+songName +"\", \"artist\":\""+artist+ "\", \"path\":\""+songPath+ "\", \"albumId\":\""+albumID+ "\", \"composer\":\""+composer+ "\", \"imageUrl\":\""+imageUrl +"\"},";
-
-                      songID++;
-
-                }
-                url = url.substring(0, url.length()-1) + "]}";
-              
-                System.out.println(""+url);
-              
-                File file = new File("filename_sunday_sus_main.txt");
-
-                 // if file doesnt exists, then create it
-                 if (!file.exists()) {
-                         file.createNewFile();
-                 }
-
-                 FileWriter fw = new FileWriter(file.getAbsoluteFile());
-                 BufferedWriter bw = new BufferedWriter(fw);
-                 bw.write(url);
-                 bw.close();
-
-                 System.out.println("Done");
-
-
-            }
-        } catch (Exception e) {
-            
-        } finally {
-            
         }
+        url = url.substring(0, url.length() - 1) + "]}";
+
+        System.out.println("" + url);
+
+        File file = new File("filename_sunday_sus_main.txt");
+
+        // if file doesnt exists, then create it
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(url);
+        bw.close();
+
+        System.out.println("Done");
 
     }
 
-    
 }
