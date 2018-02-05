fe3fe1d3113f14a366b85ee811f9edbb7e012a3f
Fahad <fahad@ipvisionsoft.com>
2018-02-05 13:45:10 +0600

    dor + kuasha + jibon

--- src/javaapplication1/GenerateTest2JibonerGolpo.java
+++ src/javaapplication1/GenerateTest2JibonerGolpo.java
@@ -145,7 +145,7 @@ public class GenerateTest2JibonerGolpo {
         String start = "12/08/2010";
         String end = "01/02/2011";
         DateTimeFormatter pattern = DateTimeFormat.forPattern("dd/mm/yyyy");
-        DateTime startDate = new DateTime(2018, 1, 26, 0, 0, 0, 0);//pattern.parseDateTime(start); // year-month-day
+        DateTime startDate = new DateTime(2018, 1, 31, 0, 0, 0, 0);//pattern.parseDateTime(start); // year-month-day
         DateTime endDate = new DateTime(2010, 1, 1, 0, 0, 0, 0);//pattern.parseDateTime(end);
 
         
