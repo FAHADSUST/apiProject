3e8e3ce448e224ce5ebd105b721d591930500156
Fahad <mdashfahad@gmail.com>
2018-06-22 20:30:37 +0600

    bhoot + kuasha

--- src/javaapplication1/GenerateTest2DK.java
+++ src/javaapplication1/GenerateTest2DK.java
@@ -147,7 +147,7 @@ public class GenerateTest2DK {
         String start = "12/08/2010";
         String end = "01/02/2011";
         DateTimeFormatter pattern = DateTimeFormat.forPattern("dd/mm/yyyy");
-        DateTime startDate = new DateTime(2018, 06, 15, 0, 0, 0, 0);//pattern.parseDateTime(start); // year-month-day
+        DateTime startDate = new DateTime(2018, 06, 22, 0, 0, 0, 0);//pattern.parseDateTime(start); // year-month-day
         DateTime endDate = new DateTime(2010, 1, 1, 0, 0, 0, 0);//pattern.parseDateTime(end);
 
         
