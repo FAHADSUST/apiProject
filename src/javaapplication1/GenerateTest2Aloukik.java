11ee0f98339e411664896c126a1b9f8c07b5455b
Fahad <mdashfahad@gmail.com>
2018-09-23 21:56:40 +0600

    sunbday

--- src/javaapplication1/GenerateTest2Aloukik.java
+++ src/javaapplication1/GenerateTest2Aloukik.java
@@ -199,7 +199,7 @@ public class GenerateTest2Aloukik {
                 compressedByte = compress(new Gson().toJson(appList));
                 String appListCompressedFullDta = Base64.getEncoder().encodeToString(compressedByte);
 
-                Data data = new Data("107", compressedFullData, appListCompressedFullDta);
+                Data data = new Data("112", compressedFullData, appListCompressedFullDta);
 
 
                 File file = new File("api-aloukik-robbar/filename_aloukik_robbar_2.json");
--- src/javaapplication1/GenerateTest2Suspense.java
+++ src/javaapplication1/GenerateTest2Suspense.java
@@ -199,7 +199,7 @@ public class GenerateTest2Suspense {
                 compressedByte = compress(new Gson().toJson(appList));
                 String appListCompressedFullDta = Base64.getEncoder().encodeToString(compressedByte);
 
-                Data data = new Data("904", compressedFullData, appListCompressedFullDta);
+                Data data = new Data("905", compressedFullData, appListCompressedFullDta);
 
                 
                 File file = new File("radio_sunday_sus/filename_sunday_sus_main_2.json");
